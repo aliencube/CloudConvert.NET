9c07971200c171ea0e908c7855e10456c364e2da
Justin Yoo <justin.yoo@aliencube.com>
2015-03-19 15:16:55 +1100

    Add test project

---
+++ SourceCodes/99_Tests/CloudConvert.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Aliencube.CloudConvert.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("aefe137a-0772-4d24-9d6a-67625fc5e506")]
